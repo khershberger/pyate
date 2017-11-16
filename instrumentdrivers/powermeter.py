d01ed51cbf2d3b88f22f93956ff5a29b1cce9f44
Kyle Hershberger <kyle.hershberger@gmail.com>
2017-11-16 16:46:37 -0700

    Implement getSettings method.
    Allows quick reading of channel settings from instrument.  Also added
    some notes about trying to read operation complete flag directly from
    a status register.

--- instrumentdrivers/powermeter.py
+++ instrumentdrivers/powermeter.py
@@ -4,8 +4,24 @@ Created on Jul 12, 2017
 @author: kyleh
 '''
 
-from .instrument import Instrument
 
+'''
+Some notes for later:
+
+This block should directly read the operation complete flag from
+the <can't remember exact name of it> status register.
+It works, but it takes a long time for the flag to be set.
+
+stat = 0
+pm.res.write('*OPC')
+pm.res.write('INIT1')
+while(not (stat & 1)):
+     stat = int(pm.res.query('*ESR?'))
+     print('{0:d} {0:b}'.format(stat, stat))
+
+'''
+
+from .instrument import Instrument
 
 @Instrument.registerModels(['E4417A'])
 class PowerMeter(Instrument):
@@ -25,6 +41,19 @@ class PowerMeter(Instrument):
 
     def setFrequency(self, channel, freq):
         self.write('SENS{:d}:FREQ {:g}HZ'.format(channel, freq))
+        
+    def getSettings(self,channel):
+        result = {}
+        result['readingsPerSecond'] = self.query('SENS{:d}:SPE?'.format(channel))
+        result['continuous']        = self.query('INIT{:d}:CONT?'.format(channel))
+        result['trigSource']        = self.query('TRIG:SOUR?'.format(channel))
+        
+        if (self.query('SENS{:d}.AVER:COUN:AUTO?'.format(channel)) == 'AUTO'):
+            result['averaging']     = 'AUTO'
+        else:
+            result['averaging']     = self.query('SENS{:d}:AVER:COUN?'.format(channel))
+        
+        return result  
         
     def setSettings(self, channel, 
                     readingsPerSecond=40,
