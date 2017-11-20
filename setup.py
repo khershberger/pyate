35a7d7f84e0a05beeeef9aa1a672545a3af4c6c3
Kyle Hershberger <kyle.hershberger@gmail.com>
2017-11-20 18:53:23 -0500

    Update setup.py
    Renamed package from pate to pyate
    Added numpy ddpendency

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name = 'PATE',
+setup(name = 'PyATE',
       description = 'Python Autmoated Test Environment',
       authon = 'Kyle Hershberger',
-      version = '0.1.0',
-      packages = ['pate'],
+      version = '0.1.1',
+      packages = ['pyate'],
       install_requires = [
           'matplotlib',
+		  'numpy',
           'pandas',
           'pyqt5',
           'pyvisa',
