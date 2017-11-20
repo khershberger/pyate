7758830fd5a41ea8d7408a40786a44e863b6cdd3
Kyle Hershberger <kyle.hershberger@gmail.com>
2017-11-20 16:27:23 -0700

    Add documentation mind maps

---
+++ doc/PyATE.mm
@@ -0,0 +1,97 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="PyATE" FOLDED="false" ID="ID_632243881" CREATED="1510677969264" MODIFIED="1510678480014" STYLE="oval">
+<font SIZE="12"/>
+<hook NAME="MapStyle" zoom="1.771">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="visawrapper" POSITION="right" ID="ID_802914271" CREATED="1510678548946" MODIFIED="1510678568686">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="pyate-gui" POSITION="right" ID="ID_169389097" CREATED="1510678035003" MODIFIED="1510678511508">
+<edge COLOR="#ff0000"/>
+<font SIZE="12"/>
+</node>
+<node TEXT="pyate-instrument" POSITION="right" ID="ID_1322476833" CREATED="1510678043811" MODIFIED="1510678505595">
+<edge COLOR="#0000ff"/>
+<font SIZE="12"/>
+<node TEXT="manager" ID="ID_489572557" CREATED="1510782001108" MODIFIED="1510782004225">
+<node TEXT="InstrumentManager" ID="ID_973552064" CREATED="1510782004738" MODIFIED="1510782011908">
+<node TEXT="visawrapper" ID="ID_1906854750" CREATED="1510782013682" MODIFIED="1510782015825">
+<node TEXT="Resource" ID="ID_1018071937" CREATED="1510782079882" MODIFIED="1510782081954">
+<node TEXT="prologix" ID="ID_1787357331" CREATED="1510782027423" MODIFIED="1510782029600">
+<node TEXT="interface" ID="ID_1714118219" CREATED="1510782030079" MODIFIED="1510782033666">
+<node TEXT="bus" ID="ID_718555849" CREATED="1510782034001" MODIFIED="1510782035122"/>
+</node>
+</node>
+<node TEXT="pyvisa" ID="ID_183629966" CREATED="1510782037056" MODIFIED="1510782038834"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="pyate-modules" POSITION="right" ID="ID_1375851758" CREATED="1510678045906" MODIFIED="1510678499452">
+<edge COLOR="#00ff00"/>
+<font SIZE="12"/>
+</node>
+</node>
+</map>
