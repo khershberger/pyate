20e4a02deeea0e8ad4bb557b18cf83dbb3c992ae
kyleh <kyleh@gianfar.net>
2017-12-04 19:48:52 -0500

    Add combined setSettings and DC current measurement

--- pyate/instrument/multimeter.py
+++ pyate/instrument/multimeter.py
@@ -12,9 +12,31 @@ class Multimeter(Instrument):
         super().__init__(*args, **kwargs)
         self.drivername = 'Multimeter'
         
-    def measCurrentDC(self):
-        #self.write('INST:SEL OUT1')
-        result = self.query('MEAS:CURR:DC?')
+    def setSettings(self,  
+                    mode='DCV',
+                    trigSource='IMM',
+                    filter=20):
+        
+        
+        self.write('DET:BAND {:g}'.format(filter))
+        self.write('TRIG:SOUR {:s}'.format(trigSource))
+        
+        
+        if (averaging == 'AUTO'):
+            self.write('SENS{:d}:AVER:COUN:AUTO 1'.format(channel))
+        elif (averaging == 0):
+            self.write('SENS{:d}:AVER 0'.format(channel))
+        else:
+            self.write('SENS{:d}:AVER:COUN {:d}'.format(channel,averaging))
+        
+    def measCurrentDC(self, mode='MEAS'):
+        if (mode == 'MEAS'):
+            result = self.query('MEAS:CURR:DC?')
+        elif (mode == 'READ'):
+            result = self.query('READ?')
+        else:
+            raise Exception('Unknown measurement mode {:s}'.format(mode))
+            
         try:
             value = float(result)
         except ValueError as e:
