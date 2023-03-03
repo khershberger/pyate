ea25cf6298ece432e7d13b0b52a239c59930b075
Kyle Hershberger <kyle.hershberger@gmail.com>
2023-03-03 14:38:20 -0500

    Improved open_resource implementation

--- pyate/visawrapper/visawrapper.py
+++ pyate/visawrapper/visawrapper.py
@@ -6,6 +6,9 @@ import logging
 import pyvisa
 from . import prologix
 
+from pyvisa.errors import VisaIOError
+from pyvisa.constants import StatusCode
+
 
 class InitializationError(Exception):
     pass
@@ -18,9 +21,9 @@ class ResourceTimeout(Exception):
 class ResourceManager:
     """
     Singleton to store resource instances
-    
+
     Attributes:
-    
+
     """
 
     resourcemanagers = {}
@@ -48,7 +51,7 @@ class ResourceManager:
         return cls.resourcemanagers[rmtype]
 
     @classmethod
-    def open_resource(cls, resource_name, backend="@py", **kwargs):
+    def open_resource_old(cls, resource_name, backend="@py", **kwargs):
         logger = logging.getLogger(__name__)
         # First see if this resource_name is already created
         if resource_name in cls.resources:
@@ -78,11 +81,51 @@ class ResourceManager:
         return resource
 
     @classmethod
-    def addResource(cls, name, newResource):
-        if name in cls.resources:
-            raise KeyError(name + " already exists")
+    def open_resource(cls, resource_name, backend="@py", **kwargs):
+        logger = logging.getLogger(__name__)
+
+        # rm.visalib.open_default_resource_manager
+
+        if backend == "@prologix":
+            # Create prologix resource
+            rm = cls.getResourceManager("prologix")
+        elif backend == "@py":
+            rm = cls.getResourceManager("pyvisa-py")
         else:
-            cls.resources[name] = newResource
+            # Create pyvisa rsource
+            rm = cls.getResourceManager("pyvisa")
+
+        # Check if it is already open
+        resource_valid = False
+        for resource in rm.list_opened_resources():
+            # Check if it is what we want
+            try:
+                if resource_name == resource.resource_name:
+                    logger.debug("Resource %s found in opened_resources list", resource_name)
+                    resource_valid = True
+                    break
+            except VisaIOError as e:
+                if e.error_code == StatusCode.error_invalid_object:
+                    logger.debug("Resource %s found in opened_resources list but is invalid", resource_name)
+                    # Bad session
+                    break
+                else:
+                    raise
+
+        if not resource_valid:
+            logger.info("Creating new resource: {:s}".format(resource_name))
+
+            resource = rm.open_resource(resource_name, **kwargs)
+            cls.addResource(resource_name, resource)
+        
+        return resource
+
+    @classmethod
+    def addResource(cls, name, newResource):
+        # if name in cls.resources:
+        #     raise KeyError(name + " already exists")
+        # else:
+        cls.resources[name] = newResource
 
     @classmethod
     def getResources(cls):
@@ -102,4 +145,5 @@ class ResourceManager:
             try:
                 cls.resources[keyRes].close()
             except pyvisa.VisaIOError:
-                logger.warning("VisaIOError occured while trying to close {:s}".format(keyRes))
+                logger.warning(
+                    "VisaIOError occured while trying to close {:s}".format(keyRes))
